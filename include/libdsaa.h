5e7f168cb2cf1a5adaf84b9de687fcdaa56a336c
root <root@localhost>
2018-06-28 00:37:52 +0200

    Added definitions of functions.

--- include/libdsaa.h
+++ include/libdsaa.h
@@ -24,4 +24,15 @@ struct list {
 	int size;
 };
 
+void list_init(struct list*, struct list_function*);
+int list_add(struct list*, void*);
+int list_add_sort(struct list*, void*);
+int list_find(struct list*, void*);
+int list_move(struct list*, void* i, list_item_position, void*);
+int list_remove(struct list*, void*);
+int list_print(struct list*, list_item_position);
+int list_release(struct list*);
+
+
+
 #endif
